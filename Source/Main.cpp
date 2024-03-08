3a29327c271b20490b57a3d5ae7991ad1eb23ba1
ayan-acharya <ayan.acharya02@gmail.com>
2024-03-08 21:16:20 +0530

    Simple triangle

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -25,8 +25,16 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Triangle
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
 
         /* Poll for and process events */
         glfwPollEvents();
